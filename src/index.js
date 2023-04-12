7412b876ec8b699663e3febba8ab43df1d1fc0fb
Mitchell Gantz <mgantz22@icloud.com>
2023-04-12 16:06:16 -0700

    end of day commit

--- src/index.js
+++ src/index.js
@@ -4,22 +4,44 @@ import "./css/styles.css";
 import * as character from "./js/character.js";
 
 //User Interface Logic
+let currCharacter;
+
+const toggleCharacterAssets = (curChar) => {
+  document.getElementById('heroName').innerHTML = curChar().name;
+  document.getElementById('heroLevel').innerHTML = curChar().
+}
+
+const handleWizard = (event) => {
+  event.preventDefault();
+  currCharacter = character.storeState("Wizard");
+  character.createWizard(currCharacter);
+};
+
 
 window.onload = function () {
-  document.getElementById("wizard").onclick = function () {
-    const newState = character.stateControlCharacter(character.incAttack);
-    document.getElementById("wizard-attack").innerText = `Character Stats: ${newState.attack}`;
+  document.getElementById("wizardState").onclick = function () {
+    const currentState = character.stateControlWizard();
+    const wizardAttack = character.stateControlWizard(character.incAttack);
+    const wizardHealth = character.stateControlWizard(character.incHealth);
+    const warriorHealthDecrease = character.stateControlWizard(character.decHealth)(character.magic);
+    const wizardMana = character.stateControlWizard(character.decMana)(character.magic);
+    document.getElementById("wizard-attackstat").innerText = `${wizardAttack.attack}`;
+    document.getElementById("wizard-health").innerText = `${wizardHealthDecrease.health}`;
+    // document.getElementById("wizard-magic").innerText = `${wizardAttack.attack}`;
+    document.getElementById("wizard-mana").innerText = `${wizardMana.mana}`;
+    console.log("wizardState");
+
   };
 
-  document.getElementById("show-state").onclick = function () {
-    const currentState = character.stateControlCharacter();
-    document.getElementById("wizard-attack").innerText = `Attack: ${currentState.attack}`;
-  };
+  // document.getElementById("show-state").onclick = function () {
+  //   const currentState = character.stateControlWizard();
+  //   document.getElementById("wizardState").innerText = `Wizard: ${currentState.attack}`;
+  // };
 };
 
 window.addEventListener("load", function () {
   document
     .querySelector("form")
     .addEventListener("submit");
-  // stateControlCharacter();
+  stateControlWizard();
 });
--- src/js/character.js
+++ src/js/character.js
@@ -11,7 +11,7 @@ export const storeState = (name) => {
 
 // export const stateControl = storeState();
 
-export const stateControlCharacter = storeState();
+export const stateControlWizard = storeState();
 
 
 const incrementState = (prop) => {
@@ -23,16 +23,16 @@ const incrementState = (prop) => {
   };
 };
 
-export const incLevel = incrementState("level")(1);
-export const incMaxHealth = incrementState("maxHealth")(20);
-export const incHealth = incrementState("health")(10);
-export const incAttack = incrementState("attack")(5);
-export const incMagic = incrementState("magic")(5);
-export const incMaxMana = incrementState("maxMana")(20);
-export const incMana = incrementState("mana")(5);
-export const incExp = incrementState("exp")(5);
-export const incAttackPercent = incrementState("atkPercent")(10);
-export const incMagicPercent = incrementState("magPercent")(15);
+export const incLevel = incrementState("level");
+export const incMaxHealth = incrementState("maxHealth");
+export const incHealth = incrementState("health");
+export const incAttack = incrementState("attack");
+export const incMagic = incrementState("magic");
+export const incMaxMana = incrementState("maxMana");
+export const incMana = incrementState("mana");
+export const incExp = incrementState("exp");
+export const incAttackPercent = incrementState("atkPercent");
+export const incMagicPercent = incrementState("magPercent");
 
 const decrementState = (prop) => {
   return (value) => {
@@ -43,18 +43,17 @@ const decrementState = (prop) => {
   };
 };
 
-export const decHealth = decrementState("health")(-5);
-export const decAttack = decrementState("attack")(-5);
-export const decMagic = decrementState("magic")(-10);
-export const decMaxMana = decrementState("maxMana")(-5);
-export const decMana = decrementState("mana")(-1);
-export const decExp = decrementState("exp")(-2);
+export const decHealth = decrementState("health");
+export const decAttack = decrementState("attack");
+export const decMagic = decrementState("magic");
+export const decMaxMana = decrementState("maxMana");
+export const decMana = decrementState("mana");
+export const decExp = decrementState("exp");
 
 //Character Function Factory
-
-export const createWizard = (character) => {
-  // let state = {
-  character(incLevel(1));
+// character();
+export const wizard = (character) => {
+  const character = incLevel(1);
   character(incMaxHealth(20));
   character(incHealth(20));
   character(incAttack(10));
@@ -68,7 +67,9 @@ export const createWizard = (character) => {
 // return {...state, ...createWizard(stateControl(state))};
 // };
 
-export const createWorrior = (character) => {
+export const stateControlWarrior = storeState();
+
+export const createWarrior = (character) => {
   character(incLevel(1));
   character(incMaxHealth(15));
   character(incHealth(15));
@@ -82,10 +83,38 @@ export const createWorrior = (character) => {
 };
 
 export const createSkeletonKing = (character) => {
-  character(incHealth(15));
+  character(incHealth(12));
   character(incAttack(2));
   character(incExp(10));
   character(incAttackPercent(50));
+};
+
+export const createTheButcher = (character) => {
+  character(incHealth(13));
+  character(incAttack(2));
+  character(incExp(20));
+  character(incAttackPercent(60));
+};
+
+export const createMephisto = (character) => {
+  character(incHealth(15));
+  character(incAttack(2));
+  character(incExp(30));
+  character(incAttackPercent(60));
+};
+
+export const createQueenAdria = (character) => {
+  character(incHealth(20));
+  character(incAttack(2));
+  character(incExp(40));
+  character(incAttackPercent(60));
+};
+
+export const createBaal = (character) => {
+  character(incHealth(25));
+  character(incAttack(5));
+  character(incExp(50));
+  character(incAttackPercent(60));
 };
 
 export const chance = () => {
@@ -108,5 +137,24 @@ export const magic = (character) => {
     } else {
       return character().magic / 2;
     }
+  } else {
+    document.getElementById("errorAtk").innerHTML = `NOT ENOUGH MANA!`;
   }
+};
+
+export const lvlup = (character) => {
+  if (character().exp >= 50) {
+    character(decExp(50));
+    character(incLevel(1));
+    character(incMaxHealth(5));
+    character(incHealth(character().maxHealth - character().health));
+    character(incAttack(2));
+    character(incMagic(2));
+    character(incMaxMana(3));
+    character(incMana(character().maxMana- character().mana));
+  }
+}
+
+export const isDead = (character) => {
+  return character().health <= 0;
 };
